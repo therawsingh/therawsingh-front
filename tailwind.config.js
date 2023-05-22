f4ceab06135439fafb7b4e35e9ea4987550f6353
fLuKeroshan <32307382+fLuKeroshan@users.noreply.github.com>
2023-05-22 08:12:40 +0530

    tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+export const content = [
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+  "./app/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
