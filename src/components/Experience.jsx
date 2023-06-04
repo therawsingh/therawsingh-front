63a33ec4fed7c0f1e15acf1a45fc41d4d4d7b02a
fLuKeroshan <32307382+fLuKeroshan@users.noreply.github.com>
2023-06-05 02:07:15 +0530

    new updates fixes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,16 +10,19 @@ const Experience = () => {
       <div className="w-full py-6 font-josefin">
         <div className="max-w-[1920px] w-9/12 h-screen mx-auto text-left pr-10 flex items-center text-lg">
           <div className="pl-6">
-            <h6 className="text-[#ee2400] pb-4 text-sm sm:text-xl">
+            <h6 className="text-[#ee2400] pb-14 text-sm sm:text-xl">
               Experience (Hover/Tap for more)
             </h6>
             <div className="w-full">
               <Text3d
                 primary={"Capgemini"}
-                secondary={"Automation Tester Jan'22 - Jul'22"}
+                secondary={"Automation Tester Jan'22–Jul'22"}
               />
-              <Text3d primary={"Infosys"} secondary={"Automation Tester Oct'19 - Jan'22"} />
-              <Text3d primary={"Backend Developer"} secondary={"Self-Taught (Java)"} />
+              <Text3d primary={"Infosys"} secondary={"Automation Tester Oct'19–Jan'22"} />
+              <Text3d
+                primary={"Backend Developer"}
+                secondary={"Self-Taught (Java)"}
+              />
               <Text3d
                 primary={"Frontend Developer"}
                 secondary={"Self-Taught (ReactJS)"}
@@ -34,9 +37,13 @@ const Experience = () => {
 
 function Text3d({ primary, secondary }) {
   return (
-    <div className="textContainer">
-      <p className="primary w-full pb-5">{primary}</p>
-      <p className="secondary w-full pb-1">{secondary}</p>
+    <div className="textContainer m-2 leading-loose">
+      <p className="primary w-full pb-12 sm:pb-8 lg:pb-6 md:text-5xl sm:text-4xl text-2xl">
+        {primary}
+      </p>
+      <p className="secondary w-full md:text-5xl sm:text-4xl text-2xl pb-4 pt-4 sm:pt-0">
+        {secondary}
+      </p>
     </div>
   );
 }
