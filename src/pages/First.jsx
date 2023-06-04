35c1c7e73b435dc3ed473a5cc0672845d94d75bc
fLuKeroshan <32307382+fLuKeroshan@users.noreply.github.com>
2023-06-05 01:00:35 +0530

    fixe code

--- src/pages/First.jsx
+++ src/pages/First.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Me from "../components/Me";
 import Experience from "../components/Experience";
-import ExperienceTwo from "../components/ExperienceTwo";
 import Landing from "../components/Landing";
 import Skills from "../components/Skills";
 import Website from "../components/Website";
@@ -11,9 +10,7 @@ import Footer from "../components/Footer";
 const First = () => {
   return (
     <div>
-
-        <Navbar />
-
+      <Navbar />
       <Landing />
       <Me />
       <Skills />
